cef6de7a341ec3eab490dedf427808fb57f7edb8
SunZhichao <sunzhichao_cs@163.com>
2024-09-21 21:13:40 +0800

    feat:基于SPI机制，加载配置中心管理器，订阅配置变化

--- LiteGateway-Core/src/main/java/me/raining/gateway/core/Bootstrap.java
+++ LiteGateway-Core/src/main/java/me/raining/gateway/core/Bootstrap.java
@@ -1,6 +1,10 @@
 package me.raining.gateway.core;
 
 import lombok.extern.slf4j.Slf4j;
+import me.raining.gateway.config.center.api.ConfigCenter;
+
+import java.util.ServiceLoader;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author raining
@@ -13,5 +17,99 @@ public class Bootstrap {
         //加载网关核心静态配置
         Config config = ConfigLoader.getInstance().load(args);
         System.out.println(config.getPort());
+
+        //配置中心相关
+        //基于SPI机制，加载配置中心管理器
+        ServiceLoader<ConfigCenter> serviceLoader = ServiceLoader.load(ConfigCenter.class);
+        //默认使用第一个实现
+        final ConfigCenter configCenter = serviceLoader.findFirst().orElseThrow(() -> {
+            log.error("not found ConfigCenter impl");
+            return new RuntimeException("not found ConfigCenter impl");
+        });
+
+        //初始化配置中心(里面会初始化rules)
+        configCenter.init(config.getRegistryAddress(), config.getEnv());
+        //监听配置的新增、修改、删除
+        //一旦有变化，就更新规则rules集合，目前先拿到rule集合，并打印，看一下效果
+        //测试的时候，需要在nacos下创建配置：
+        //Data Id: api-gateway；Group: dev
+        //json文件：
+        /**
+         * {
+         *   "rules": [
+         *     {
+         *       "id": "user-private",
+         *       "name": "user-private",
+         *       "paths": [
+         *         "/user/private/user-info"
+         *       ],
+         *       "prefix": "/user/private",
+         *       "protocol": "http",
+         *       "serviceId": "backend-user-server",
+         *       "filterConfigs": [
+         *         {
+         *           "config": {
+         *             "load_balance": "Random"
+         *           },
+         *           "id": "load_balance_filter"
+         *         },
+         *         {
+         *           "id":"auth_filter"
+         *         }
+         *       ]
+         *     },
+         *     {
+         *       "id": "user",
+         *       "name": "user",
+         *       "paths": [
+         *         "/user/login"
+         *       ],
+         *       "prefix": "/user",
+         *       "protocol": "http",
+         *       "serviceId": "backend-user-server",
+         *       "filterConfigs": [
+         *         {
+         *           "config": {
+         *             "load_balance": "Random"
+         *           },
+         *           "id": "load_balance_filter"
+         *         }
+         *       ]
+         *     },
+         *     {
+         *       "id": "http-server",
+         *       "name": "http-server",
+         *       "paths": [
+         *         "/http-server/ping"
+         *       ],
+         *       "prefix": "/http-server",
+         *       "protocol": "http",
+         *       "retryConfig": {
+         *         "times": 3
+         *       },
+         *       "serviceId": "backend-http-server",
+         *       "filterConfigs": [
+         *         {
+         *           "config": {
+         *             "load_balance": "RoundRobin"
+         *           },
+         *           "id": "load_balance_filter"
+         *         },
+         *         {
+         *           "id": "auth_filter"
+         *         }
+         *       ]
+         *     }
+         *   ]
+         * }
+         */
+        configCenter.subscribeRulesChange(rules -> log.info(rules.toString()));
+
+        //等待，此刻在nacos端修改rules，观察打印效果
+        try {
+            new CountDownLatch(2).await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
