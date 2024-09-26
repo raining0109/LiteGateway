3f92c205839e54103dff33d16daa5fc8aecb57dd
SunZhichao <sunzhichao_cs@163.com>
2024-09-26 19:24:26 +0800

    feat:完成注册中心API的nacos实现:NacosRegisterCenter

---
+++ LiteGateway-Common/src/main/java/me/raining/gateway/common/constant/GatewayConst.java
@@ -0,0 +1,22 @@
+package me.raining.gateway.common.constant;
+
+/**
+ * @author raining
+ * @version 1.0.0
+ * @description 网关常量
+ */
+public interface GatewayConst {
+
+    String UNIQUE_ID = "uniqueId";
+
+    String DEFAULT_VERSION = "1.0.0";
+
+    String PROTOCOL_KEY = "protocol";
+
+    int DEFAULT_WEIGHT = 100;
+
+    String META_DATA_KEY = "meta";
+
+    String BUFFER_TYPE_PARALLEL ="parallel" ;
+
+}
