dee1e006a71d3f160a1e425e6f3d7d36dd6fc0a5
SunZhichao <sunzhichao_cs@163.com>
2024-09-27 14:02:01 +0800

    feat:更新动态服务缓存配置管理类DynamicConfigManager，增加了对服务定义和服务实例的管理

--- LiteGateway-Common/src/main/java/me/raining/gateway/common/config/DynamicConfigManager.java
+++ LiteGateway-Common/src/main/java/me/raining/gateway/common/config/DynamicConfigManager.java
@@ -1,8 +1,10 @@
 package me.raining.gateway.common.config;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author raining
@@ -19,6 +21,13 @@ public class DynamicConfigManager {
     private ConcurrentHashMap<String /* 路径 */ , Rule> pathRuleMap = new ConcurrentHashMap<>();
     //一个服务对应着多个规则
     private ConcurrentHashMap<String /* 服务名 */ , List<Rule>> serviceRuleMap = new ConcurrentHashMap<>();
+
+    //服务
+    //	服务的定义集合：uniqueId代表服务的唯一标识
+    private ConcurrentHashMap<String /* uniqueId */ , ServiceDefinition>  serviceDefinitionMap = new ConcurrentHashMap<>();
+
+    //	服务的实例集合：uniqueId与一对服务实例对应
+    private ConcurrentHashMap<String /* uniqueId */ , Set<ServiceInstance>>  serviceInstanceMap = new ConcurrentHashMap<>();
 
     /**
      * 私有化构造方法
@@ -84,6 +93,81 @@ public class DynamicConfigManager {
 
     public List<Rule> getRuleByServiceId(String serviceId) {
         return serviceRuleMap.get(serviceId);
+    }
+
+
+    /***************** 	对服务定义缓存进行操作的系列方法 	***************/
+
+    public void putServiceDefinition(String uniqueId,
+                                     ServiceDefinition serviceDefinition) {
+        serviceDefinitionMap.put(uniqueId, serviceDefinition);;
+    }
+
+    public ServiceDefinition getServiceDefinition(String uniqueId) {
+        return serviceDefinitionMap.get(uniqueId);
+    }
+
+    public void removeServiceDefinition(String uniqueId) {
+        serviceDefinitionMap.remove(uniqueId);
+    }
+
+    public ConcurrentHashMap<String, ServiceDefinition> getServiceDefinitionMap() {
+        return serviceDefinitionMap;
+    }
+
+    /***************** 	对服务实例缓存进行操作的系列方法 	***************/
+
+    public Set<ServiceInstance> getServiceInstanceByUniqueId(String uniqueId, boolean gray){
+        Set<ServiceInstance> serviceInstances = serviceInstanceMap.get(uniqueId);
+        if (CollectionUtils.isEmpty(serviceInstances)) {
+            return Collections.emptySet();
+        }
+        //不为空且为灰度流量
+        if (gray) {
+            return  serviceInstances.stream()
+                    .filter(ServiceInstance::isGray)
+                    .collect(Collectors.toSet());
+        }
+
+        return serviceInstances;
+    }
+
+    public void addServiceInstance(String uniqueId, ServiceInstance serviceInstance) {
+        Set<ServiceInstance> set = serviceInstanceMap.get(uniqueId);
+        set.add(serviceInstance);
+    }
+
+    public void addServiceInstance(String uniqueId, Set<ServiceInstance> serviceInstanceSet) {
+        serviceInstanceMap.put(uniqueId, serviceInstanceSet);
+    }
+
+    public void updateServiceInstance(String uniqueId, ServiceInstance serviceInstance) {
+        Set<ServiceInstance> set = serviceInstanceMap.get(uniqueId);
+        Iterator<ServiceInstance> it = set.iterator();
+        while(it.hasNext()) {
+            ServiceInstance is = it.next();
+            if(is.getServiceInstanceId().equals(serviceInstance.getServiceInstanceId())) {
+                it.remove();
+                break;
+            }
+        }
+        set.add(serviceInstance);
+    }
+
+    public void removeServiceInstance(String uniqueId, String serviceInstanceId) {
+        Set<ServiceInstance> set = serviceInstanceMap.get(uniqueId);
+        Iterator<ServiceInstance> it = set.iterator();
+        while(it.hasNext()) {
+            ServiceInstance is = it.next();
+            if(is.getServiceInstanceId().equals(serviceInstanceId)) {
+                it.remove();
+                break;
+            }
+        }
+    }
+
+    public void removeServiceInstancesByUniqueId(String uniqueId) {
+        serviceInstanceMap.remove(uniqueId);
     }
 
 
