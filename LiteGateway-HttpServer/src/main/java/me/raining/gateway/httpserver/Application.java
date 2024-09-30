ff63498f323b05c36c4c78188ea3bb8504df2364
SunZhichao <sunzhichao_cs@163.com>
2024-09-30 18:16:15 +0800

    feat:HttpServer:完成HTTP服务器测试类

---
+++ LiteGateway-HttpServer/src/main/java/me/raining/gateway/httpserver/Application.java
@@ -0,0 +1,17 @@
+package me.raining.gateway.httpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author raining
+ * @version 1.0.0
+ * @description HTTP测试类
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ LiteGateway-HttpServer/src/main/java/me/raining/gateway/httpserver/controller/HttpController.java
@@ -0,0 +1,49 @@
+package me.raining.gateway.httpserver.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import me.raining.gateway.client.api.ApiInvoker;
+import me.raining.gateway.client.api.ApiProperties;
+import me.raining.gateway.client.api.ApiProtocol;
+import me.raining.gateway.client.api.ApiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author raining
+ * @version 1.0.0
+ * @description HTTP Controller类
+ */
+@Slf4j
+@RestController("/http-server")
+@ApiService(serviceId = "backend-http-server", protocol = ApiProtocol.HTTP, patternPath = "/http-server/**")
+public class HttpController {
+
+    @Resource
+    private ApiProperties apiProperties;
+
+    @ApiInvoker(path = "/http-server/ping")
+    @GetMapping("/ping")
+    public String ping() {
+        log.info("{}", apiProperties);
+        try {
+            //Thread.sleep(10000000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "this is application1";
+    }
+
+    @ApiInvoker(path = "/http-server/ping2")
+    @GetMapping("/ping2")
+    public String ping2() {
+        log.info("{}", apiProperties);
+        try {
+            //Thread.sleep(10000000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "this is ping1";
+    }
+}
